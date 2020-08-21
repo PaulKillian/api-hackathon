e91ca5fc0871d6c19be72cea4153fb83f090bdd8
PaulKillian <psk65lava@gmail.com>
2020-08-20 22:10:12 -0700

    Finishe First Step

--- main.js
+++ main.js
@@ -1,99 +1,148 @@
-//CHOOSE YOUR MAIN DISH
-
 const main = document.querySelector('main')
-const divContainer = document.createElement('div')
-divContainer.classList.add("container")
-const divRowOne = document.createElement('div')
-divRowOne.classList.add("row", "justify-content-center")
-const divRowTwo = document.createElement('div')
-divRowTwo.classList.add("row", "justify-content-center")
-const divRowThree = document.createElement('div')
-divRowThree.classList.add("row", "justify-content-center")
-const divRowFour = document.createElement('div')
-divRowFour.classList.add("row", "justify-content-center")
-const divRowFive = document.createElement('div')
-divRowFive.classList.add("row", "justify-content-center")
-const divRowSix = document.createElement('div')
-divRowSix.classList.add("row", "justify-content-center")
-const imgOne = document.createElement('img')
-imgOne.classList.add('img-thumbnail', 'w-50')
-imgOne.src = 'https://www.thespruceeats.com/thmb/APa5zYrExVw4fDRwChpwQGQuNls=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/marinated-rump-roast-3058682-hero-01-0977a498722f47debaa7034c13053048.jpg'
-const imgTwo = document.createElement('img')
-imgTwo.classList.add('img-thumbnail', 'w-50')
-imgTwo.src = 'https://www.maangchi.com/wp-content/uploads/2018/02/roasted-chicken-1.jpg'
-const imgThree = document.createElement('img')
-imgThree.classList.add('img-thumbnail', 'w-50')
-imgThree.src = 'https://images.immediate.co.uk/production/volatile/sites/2/2018/10/OLI-1018_Everyday-PanFriedCodWithGiantBeanChard_28546-acc2f54.jpg?webp=true&quality=90&crop=20px%2C3414px%2C5444px%2C2342px&resize=556%2C236'
-const h2One = document.createElement('h3')
-h2One.textContent = "Beef"
-const h2Two = document.createElement('h3')
-h2Two.textContent = "Chicken"
-const h2Three = document.createElement('h3')
-h2Three.textContent = "Fish"
+const h3R = document.querySelector('h3')
+const h2I = document.querySelector('h2')
+const h2R = document.getElementById('h2R')
+const section = document.querySelector('section')
+const imgDiv = document.getElementById('imgDiv')
+const renderPageData = {}
+let americanRecipes = {}
+let extractedDessertRecipes = []
+let arrayIngredients = []
+let arrayInstructions = []
+let pageIngredients = []
+let pageInstructions = []
+let pageInstructions2 = " "
 
-main.appendChild(divContainer)
-main.appendChild(divRowOne)
-divRowOne.appendChild(imgOne)
-main.appendChild(divRowTwo)
-divRowTwo.appendChild(h2One)
-main.appendChild(divRowThree)
-divRowThree.appendChild(imgTwo)
-main.appendChild(divRowFour)
-divRowFour.appendChild(h2Two)
-main.appendChild(divRowFive)
-divRowFive.appendChild(imgThree)
-main.appendChild(divRowSix)
-divRowSix.appendChild(h2Three)
+function renderHomePage() {
+	const divContainer = document.createElement('div')
+	divContainer.classList.add("container")
+	const divRowOne = document.createElement('div')
+	divRowOne.classList.add("row", "justify-content-center")
+	const divRowTwo = document.createElement('div')
+	divRowTwo.classList.add("row", "justify-content-center")
+	const divRowThree = document.createElement('div')
+	divRowThree.classList.add("row", "justify-content-center")
+	const divRowFour = document.createElement('div')
+	divRowFour.classList.add("row", "justify-content-center")
+	const divRowFive = document.createElement('div')
+	divRowFive.classList.add("row", "justify-content-center")
+	const divRowSix = document.createElement('div')
+	divRowSix.classList.add("row", "justify-content-center")
+	const imgOne = document.createElement('img')
+	imgOne.classList.add('img-thumbnail', 'w-50')
+	imgOne.id = "img1"
+	imgOne.src = "https://spoonacular.com/recipeImages/639114-556x370.jpg"
+	const imgTwo = document.createElement('img')
+	imgTwo.classList.add('img-thumbnail', 'w-50')
+	imgTwo.id = "img2"
+	imgTwo.src = "https://spoonacular.com/recipeImages/651958-556x370.jpg"
+	const imgThree = document.createElement('img')
+	imgThree.classList.add('img-thumbnail', 'w-50')
+	imgThree.id = "img3"
+	imgThree.src = "https://spoonacular.com/recipeImages/638038-556x370.jpg"
+	const h2One = document.createElement('h3')
+	h2One.textContent = "Dessert Recipes"
+	const h2Two = document.createElement('h3')
+	h2Two.textContent = "Breakfast Recipes"
+	const h2Three = document.createElement('h3')
+	h2Three.textContent = "Dinner Recipes"
 
+	main.appendChild(divContainer)
+	main.appendChild(divRowOne)
+	divRowOne.appendChild(imgOne)
+	main.appendChild(divRowTwo)
+	divRowTwo.appendChild(h2One)
+	main.appendChild(divRowThree)
+	divRowThree.appendChild(imgTwo)
+	main.appendChild(divRowFour)
+	divRowFour.appendChild(h2Two)
+	main.appendChild(divRowFive)
+	divRowFive.appendChild(imgThree)
+	main.appendChild(divRowSix)
+	divRowSix.appendChild(h2Three)
+}
 
-//CHOOSE YOUR RECIPE - WHEN A MAIN DISH IS CLICK THE MAIN HTML IS DESTOYED
-// const secondPage = document.getElementById('secondPage')
-// const divContainer = document.createElement('div')
-// divContainer.classList.add("container")
-// const divRowOne = document.createElement('div')
-// divRowOne.classList.add("row", "justify-content-center")
-// const divRowTwo = document.createElement('div')
-// divRowTwo.classList.add("row", "justify-content-center")
-// const divRowThree = document.createElement('div')
-// divRowThree.classList.add("row", "justify-content-center")
-// const divRowFour = document.createElement('div')
-// divRowFour.classList.add("row", "justify-content-center")
-// const divRowFive = document.createElement('div')
-// divRowFive.classList.add("row", "justify-content-center")
-// const divRowSix = document.createElement('div')
-// divRowSix.classList.add("row", "justify-content-center")
-// const imgOne = document.createElement('img')
-// imgOne.classList.add('img-thumbnail', 'w-75')
-// imgOne.src = 'https://www.thespruceeats.com/thmb/APa5zYrExVw4fDRwChpwQGQuNls=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/marinated-rump-roast-3058682-hero-01-0977a498722f47debaa7034c13053048.jpg'
-// const imgTwo = document.createElement('img')
-// imgTwo.classList.add('img-thumbnail', 'w-75')
-// imgTwo.src = 'https://www.maangchi.com/wp-content/uploads/2018/02/roasted-chicken-1.jpg'
-// const imgThree = document.createElement('img')
-// imgThree.classList.add('img-thumbnail', 'w-75')
-// imgThree.src = 'https://images.immediate.co.uk/production/volatile/sites/2/2018/10/OLI-1018_Everyday-PanFriedCodWithGiantBeanChard_28546-acc2f54.jpg?webp=true&quality=90&crop=20px%2C3414px%2C5444px%2C2342px&resize=556%2C236'
-// const h2One = document.createElement('h2')
-// h2One.textContent = "Beef"
-// const h2Two = document.createElement('h2')
-// h2Two.textContent = "Chicken"
-// const h2Three = document.createElement('h2')
-// h2Three.textContent = "Fish"
+renderHomePage()
 
-// secondPage.appendChild(divContainer)
-// secondPage.appendChild(divRowOne)
-// divRowOne.appendChild(imgOne)
-// secondPage.appendChild(divRowTwo)
-// divRowTwo.appendChild(h2One)
-// secondPage.appendChild(divRowThree)
-// divRowThree.appendChild(imgTwo)
-// secondPage.appendChild(divRowFour)
-// divRowFour.appendChild(h2Two)
-// secondPage.appendChild(divRowFive)
-// divRowFive.appendChild(imgThree)
-// secondPage.appendChild(divRowSix)
-// divRowSix.appendChild(h2Three)
+const imgOne = document.getElementById('img1')
+const imgTwo = document.getElementById('img2')
+const imgThree = document.getElementById('img3')
+imgOne.addEventListener('click', getExtractedRandomDessertRecipes)
+imgTwo.addEventListener('click', getExtractedRandomDinnerRecipes)
+imgThree.addEventListener('click', getExtractedRandomBreakfastRecipes)
 
+function renderRecipeIngredientPage(data) {
+	extractedDessertRecipes = data
+	arrayInstructions = extractedDessertRecipes.recipes[0].analyzedInstructions[0].steps
+	arrayIngredients = extractedDessertRecipes.recipes[0].extendedIngredients
+	for (let i = 0; i < arrayIngredients.length; i++) {
+		pageIngredients.push(arrayIngredients[i].name)
+	}
+	for (let i = 0; i < arrayInstructions.length; i++) {
+		pageInstructions.push(arrayInstructions[i].step)
+	}
 
-// const newMainImages = new MainImages()
+	const ul = document.createElement('ul')
+	for (let i = 0; i < pageInstructions.length; i++) {
+		const li = document.createElement('li')
+		li.textContent = pageInstructions[i]
+		ul.appendChild(li)
+	}
+	main.innerHTML = " "
+	h2R.textContent = "Here are your ingredients!"
+	ul.appendChild(h2R)
+	h3R.textContent = pageInstructions2
+	h2I.textContent = "Here is your recipe!"
+	for (let i = 0; i < pageIngredients.length; i++) {
+		const li = document.createElement('li')
+		li.textContent = pageIngredients[i]
+		ul.appendChild(li)
+	}
 
-const newApp = new App()
-newApp.start()
+	const image = document.createElement('img')
+	image.src = extractedDessertRecipes.recipes[0].image
+	image.classList.add("mt-10", "mb-3")
+	imgDiv.appendChild(image)
+	h3R.textContent = extractedDessertRecipes.recipes[0].title
+
+	main.appendChild(ul)
+}
+
+	function getExtractedRandomDessertRecipes() {
+		$.ajax({
+			type: "GET",
+			url: "https://api.spoonacular.com/recipes/random?number=1&tags=dessert&apiKey=6f594a794c9e4c2b89c66311b4b9c999",
+			contentType: "application/json",
+			dataType: "json",
+
+			success: function (data) {
+				renderRecipeIngredientPage(data)
+			}
+		})
+}
+
+	function getExtractedRandomDinnerRecipes() {
+		$.ajax({
+			type: "GET",
+			url: "https://api.spoonacular.com/recipes/random?number=1&tags=breakfast&apiKey=6f594a794c9e4c2b89c66311b4b9c999",
+			contentType: "application/json",
+			dataType: "json",
+
+			success: function (data) {
+				renderRecipeIngredientPage(data)
+			}
+		})
+}
+
+	function getExtractedRandomBreakfastRecipes() {
+		$.ajax({
+			type: "GET",
+			url: "https://api.spoonacular.com/recipes/random?number=2&tags=dinner&apiKey=6f594a794c9e4c2b89c66311b4b9c999",
+			contentType: "application/json",
+			dataType: "json",
+
+			success: function (data) {
+				renderRecipeIngredientPage(data)
+			}
+		})
+}
